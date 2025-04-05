51002b5b4cfd06fa28582e0cbbbce446bdad66cc
Cozdig <tank-20@list.ru>
2025-04-05 18:41:38 +0300

    HW fix 2

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,25 @@
 public class Main {
-    public static void checkYear(int year){
-        if (year < 1584){
+    public static void checkYear(int year) {
+        if (year < 1584) {
             System.out.println((year + " год не является високосным"));
-        }
-        else if (year % 100 == 0 && year % 400 != 0 || year % 4 != 0){
+        } else if (year % 100 == 0 && year % 400 != 0 || year % 4 != 0) {
             System.out.println((year + " год не является високосным"));
-        }
-        else if (year % 4 == 0 || year % 400 == 0){
+        } else if (year % 4 == 0 || year % 400 == 0) {
             System.out.println((year + " год является високосным"));
         }
 
     }
 
-    public static void checkVersion(int clientOS, int yearPhone){
+    public static void checkVersion(int clientOS, int yearPhone) {
         if (yearPhone < 2015 && clientOS == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-        else if (clientOS == 0) {
+        } else if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
 
-        if (yearPhone < 2015 && clientOS == 1){
+        if (yearPhone < 2015 && clientOS == 1) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-
-        else if (clientOS == 1) {
+        } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
@@ -40,6 +35,7 @@ public class Main {
         }
         return days;
     }
+
     public static void main(String[] args) {
         //1
         int year = 2021;
@@ -48,7 +44,7 @@ public class Main {
         //2
         int clientOS = 0;
         int yearPhone = 2020;
-        checkVersion(clientOS,yearPhone);
+        checkVersion(clientOS, yearPhone);
 
         //3
         int deliveryDistance = 100;
