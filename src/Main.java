64d0d51e3f7c9eb8781cc57d4f32961458d9e59a
Cozdig <tank-20@list.ru>
2025-04-05 18:40:16 +0300

    HW fix

--- src/Main.java
+++ src/Main.java
@@ -29,21 +29,16 @@ public class Main {
         }
     }
 
-    public static void checkDelivery(int deliveryDistance, int days){
-        if (deliveryDistance < 20){
-            System.out.println("Потребуется дней: " + days);
+    public static int checkDelivery(int deliveryDistance) {
+        int days = 0;
+        if (deliveryDistance < 20) {
+            days = 1;
+        } else if (deliveryDistance < 60) {
+            days = days + 2;
+        } else if (deliveryDistance < 100) {
+            days = days + 3;
         }
-        else if (deliveryDistance < 60){
-            int upDays = days + 1;
-            System.out.println("Потребуется дней: " + upDays);
-        }
-        else if (deliveryDistance < 100){
-            int upDays = days + 2;
-            System.out.println("Потребуется дней: " + upDays);
-        }
-        else {
-            System.out.println("Доставки нет");
-        }
+        return days;
     }
     public static void main(String[] args) {
         //1
@@ -56,9 +51,8 @@ public class Main {
         checkVersion(clientOS,yearPhone);
 
         //3
-        int deliveryDistance = 95;
-        int days = 1;
-        checkDelivery(deliveryDistance,days);
+        int deliveryDistance = 100;
+        System.out.println(checkDelivery(deliveryDistance));
 
     }
 }
