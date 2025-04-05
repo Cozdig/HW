e82a18e7fe9207409cbb43292869f251e10fa3ef
Cozdig <tank-20@list.ru>
2025-04-05 15:42:30 +0300

    HW done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,64 @@
 public class Main {
+    public static void checkYear(int year){
+        if (year < 1584){
+            System.out.println((year + " год не является високосным"));
+        }
+        else if (year % 100 == 0 && year % 400 != 0 || year % 4 != 0){
+            System.out.println((year + " год не является високосным"));
+        }
+        else if (year % 4 == 0 || year % 400 == 0){
+            System.out.println((year + " год является високосным"));
+        }
+
+    }
+
+    public static void checkVersion(int clientOS, int yearPhone){
+        if (yearPhone < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+        if (yearPhone < 2015 && clientOS == 1){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+        else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void checkDelivery(int deliveryDistance, int days){
+        if (deliveryDistance < 20){
+            System.out.println("Потребуется дней: " + days);
+        }
+        else if (deliveryDistance < 60){
+            int upDays = days + 1;
+            System.out.println("Потребуется дней: " + upDays);
+        }
+        else if (deliveryDistance < 100){
+            int upDays = days + 2;
+            System.out.println("Потребуется дней: " + upDays);
+        }
+        else {
+            System.out.println("Доставки нет");
+        }
+    }
     public static void main(String[] args) {
-        // 1
-        String firstName = " Ivan ";
-        String lastName = "Ivanov";
-        String middleName = "Ivanovich";
-        String fullName = lastName + firstName + middleName;
-        System.out.println("Ф.И.О. сотрудника — " + fullName);
+        //1
+        int year = 2021;
+        checkYear(year);
 
         //2
-        String bigFullName = fullName.toUpperCase();
-        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + bigFullName);
+        int clientOS = 0;
+        int yearPhone = 2020;
+        checkVersion(clientOS,yearPhone);
+
         //3
-        fullName = "Иванов Семён Семёнович".replace("ё", "е");
-        System.out.println("Данные Ф.И.О. сотрудника — " + fullName);
+        int deliveryDistance = 95;
+        int days = 1;
+        checkDelivery(deliveryDistance,days);
+
     }
 }
